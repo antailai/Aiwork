c145fb0b99c6bd1804ea14bda6b1a0a6936ab94f
unknown <atl18@mails.tsinghua.edu.cn>
2018-10-27 21:48:37 +0800

    正文中不在一二级汉字表中的字符在wd字典里应该单独处理

--- src/train.py
+++ src/train.py
@@ -8,25 +8,31 @@ import re
 
 def train(data_set_path):
     text = load.load_dir(data_set_path)
-    for i in range(0, 1):
+    for i in range(len(text)):
         text[i] = re.sub(r'[^\u4e00-\u9fa5]', '', text[i])
         for o in range(len(text[i])):
             if o != len(text[i]) - 1:
                 tmp_word = text[i][o]
                 next_word = text[i][o + 1]
-                load.wd[tmp_word]['count'] += 1
-                if next_word in load.wd[tmp_word]['next_dict']:
-                    load.wd[tmp_word]['next_dict'][next_word] += 1
+                if tmp_word in load.wd:
+                    load.wd[tmp_word]['count'] += 1
+                    if next_word in load.wd[tmp_word]['next_dict']:
+                        load.wd[tmp_word]['next_dict'][next_word] += 1
+                    else:
+                        load.wd[tmp_word]['next_dict'][next_word] = 1
                 else:
-                    load.wd[tmp_word]['next_dict'][next_word] = 1
+                    continue
             else:
                 tmp_word = text[i][o]
-                load.wd[tmp_word]['count'] += 1
+                if tmp_word in load.wd:
+                    load.wd[tmp_word]['count'] += 1
+                else:
+                    continue
 
 
-if __name__ == '__main__':
-    load.load_hanzi_list("../lib/一二级汉字表.txt")
-    train("../data_set/p2016-10.txt")
-    for key in load.wd:
-        if load.wd[key]['count'] != 0:
-            print(key, load.wd[key])
+# if __name__ == '__main__':
+#     load.load_hanzi_list("../lib/一二级汉字表.txt")
+#     train("../data_set/2016-10.txt")
+#     for key in load.wd:
+#         if load.wd[key]['count'] != 0:
+#             print(key, load.wd[key])
